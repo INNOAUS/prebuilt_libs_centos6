a286f09aa10568b3dc6e01a9fcc0a6167e58b0cf
Leo An <leo@innoaus.com>
2020-02-28 22:33:29 +0900

    .

---
+++ devtools/lib64/compress.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for f in *.a ; do
+	tarfile=$f.tar.bz2
+	tar cvfj $tarfile $f
+done
+for f in *.so* ; do
+	tarfile=$f.tar.bz2
+	tar cvfj $tarfile $f
+done
---
+++ devtools/share/ace/ace-devel.sh
@@ -0,0 +1,1 @@
+export ACE_ROOT=/home/leo/git-innoaus/prebuilt_externals/centos6/devtools/share/ace
